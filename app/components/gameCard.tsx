82046dd57aafe6625a9c5bb9f21bd04944848aaf
Christian Schwarz <102796724+Christian-Schwarz2003@users.noreply.github.com>
2023-09-19 12:28:31 +0200

    add: spacing

--- app/components/gameCard.tsx
+++ app/components/gameCard.tsx
@@ -1,6 +1,6 @@
 "use client";
 import styles from "./gameCard.module.css";
-import React, { useRef, useState } from "react";
+import React, {useRef, useState} from "react";
 import Legend from "./legend";
 import PlayerSection from "./playerSection";
 
@@ -8,14 +8,26 @@ export default function GameCard() {
   const [playerCount, setPlayerCount] = useState(2);
   const playerCountInputRef = useRef<HTMLInputElement>(null);
   const handlePlayerCountChange = () => {
-    const playerCount = parseInt(playerCountInputRef.current?.value||"0");
-    
+    const playerCount = parseInt(playerCountInputRef.current?.value || "0");
+
     setPlayerCount(playerCount);
-  }
+  };
   return (
-    <div>
-      <label htmlFor="playerCount"> PlayerCount</label>
-      <input type="number" min={0} max={10} defaultValue={2} id="playerCount" ref={playerCountInputRef} onChange={handlePlayerCountChange}/>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <span>
+          <label htmlFor="playerCount"> PlayerCount</label>
+          <input
+            type="number"
+            min={0}
+            max={10}
+            defaultValue={2}
+            id="playerCount"
+            ref={playerCountInputRef}
+            onChange={handlePlayerCountChange}
+          />
+        </span>
+      </div>
       <div className={styles.gameCard}>
         <Legend></Legend>
         {new Array(playerCount).fill(0).map((_, index) => (
